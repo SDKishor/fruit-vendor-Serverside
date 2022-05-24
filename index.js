22049a1849f2913252e869474da42b3aa1a81e4d
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-24 15:56:48 +0600

    single id get api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,14 @@ async function run() {
       const fruits = await cursor.toArray();
 
       res.send(fruits);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const fruit = await fruitscollection.findOne(query);
+
+      res.send(fruit);
     });
   } finally {
   }
